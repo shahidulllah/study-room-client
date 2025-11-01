69ad2485d97f29389a5d927248eb8dcda0f0f864
shahidulllah <mdshahidsumon177@gmail.com>
2025-11-01 20:02:46 +0600

    code refector

--- src/Pages/Assignment.jsx
+++ src/Pages/Assignment.jsx
@@ -9,137 +9,202 @@ import { Link, useLoaderData } from "react-router-dom";
 import Swal from "sweetalert2";
 import { AuthContext } from "../Components/AuthProvider";
 
-
 const Assignment = () => {
-    const { user } = useContext(AuthContext)
-    const assignments = useLoaderData();
-    console.log(assignments);
+  const { user } = useContext(AuthContext);
+  const assignments = useLoaderData();
+  console.log(assignments);
 
+  const [displayAssign, setDisplayAssign] = useState(assignments);
+  console.log(displayAssign);
 
-    const [displayAssign, setDisplayAssign] = useState(assignments);
-    console.log(displayAssign);
-
-    const handleFilter = e => {
-
-        console.log(e)
-        if (e === "All") {
-            setDisplayAssign(assignments);
-
-        }
-
-        else if (e === "Easy") {
-            const easyAssignment = assignments.filter(a => a.difficultyLevel === "Easy");
-            setDisplayAssign(easyAssignment);
-        }
-        else if (e === "Medium") {
-            const mediumAssignment = assignments.filter(a => a.difficultyLevel === "Medium");
-            setDisplayAssign(mediumAssignment);
-        }
-        else if (e === "Hard") {
-            const hardAssignment = assignments.filter(a => a.difficultyLevel === "Hard");
-            setDisplayAssign(hardAssignment);
-        }
+  const handleFilter = (e) => {
+    console.log(e);
+    if (e === "All") {
+      setDisplayAssign(assignments);
+    } else if (e === "Easy") {
+      const easyAssignment = assignments.filter(
+        (a) => a.difficultyLevel === "Easy"
+      );
+      setDisplayAssign(easyAssignment);
+    } else if (e === "Medium") {
+      const mediumAssignment = assignments.filter(
+        (a) => a.difficultyLevel === "Medium"
+      );
+      setDisplayAssign(mediumAssignment);
+    } else if (e === "Hard") {
+      const hardAssignment = assignments.filter(
+        (a) => a.difficultyLevel === "Hard"
+      );
+      setDisplayAssign(hardAssignment);
     }
+  };
 
+  const handleDelete = (id, userEmail) => {
+    if (userEmail !== user.email) {
+      Swal.fire({
+        icon: "error",
+        title: "Sorrrry...",
+        text: "You can't delete this!",
+        footer:
+          '<p className="text-red-600">This is not your assignment. <br/> You can only delete the assignment which is created by you.</p>',
+      });
+    } else {
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          fetch(`${import.meta.env.VITE_API_URL}/assignments/${id}`, {
+            method: "DELETE",
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              console.log(data);
+              if (data.deletedCount > 0) {
+                Swal.fire({
+                  title: "Success!",
+                  text: "Your Item has been seccessfully deleted.",
+                  icon: "success",
+                });
 
-    const handleDelete = (id, userEmail) => {
-        if ( userEmail !== user.email) {
-            Swal.fire({
-                icon: "error",
-                title: "Sorrrry...",
-                text: "You can't delete this!",
-                footer: '<p className="text-red-600">This is not your assignment. <br/> You can only delete the assignment which is created by you.</p>'
+                const remaining = assignments.filter((a) => a._id !== id);
+                setDisplayAssign(remaining);
+              }
             });
         }
-      else { Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
-        }).then((result) => {
-            if (result.isConfirmed) {
-
-                fetch(`${import.meta.env.VITE_API_URL}/assignments/${id}`, {
-                    method: 'DELETE',
-                })
-                    .then(res => res.json())
-                    .then(data => {
-                        console.log(data)
-                        if (data.deletedCount > 0) {
-                            Swal.fire({
-                                title: "Success!",
-                                text: "Your Item has been seccessfully deleted.",
-                                icon: "success"
-                            });
-
-                            const remaining = assignments.filter(a => a._id !== id);
-                            setDisplayAssign(remaining);
-                        }
-                    })
-            }
-        });}
+      });
     }
-    return (
-        <div className="p-4 lg:mx-24">
-            <div className="flex flex-col items-center justify-center  pb-10 space-y-5">
-                <h1 className="text-3xl lg:text-4xl font-bold text-center">Assignments</h1>
-                <p className="text-center lg:w-5/12 text-gray-500">Most interesting part to have the fresh mind by keeping our product. Doing assignment is the way to live and refresh your mind.</p>
+  };
+  return (
+    <div className="p-4 lg:mx-24">
+      <div className="flex flex-col items-center justify-center  pb-10 space-y-5">
+        <h1 className="text-3xl lg:text-4xl font-bold text-center">
+          Assignments
+        </h1>
+        <p className="text-center lg:w-5/12 text-gray-500">
+          Most interesting part to have the fresh mind by keeping our product.
+          Doing assignment is the way to live and refresh your mind.
+        </p>
+      </div>
+      {/* Filter */}
+      <div className="flex justify-center">
+        <div className="flex justify-center w-6/12 divide-x rounded bg-violet-400 text-gray-800 divide-gray-700">
+          <button
+            type="button"
+            className="px-4 lg:px-8 lg:py-3 font-semibold text-lg flex items-center gap-3"
+          >
+            <PiUserList className="text-2xl hidden" /> Filtered Assignments
+          </button>
+          <button type="button" title="Toggle dropdown" className="p-3">
+            <div className="dropdown">
+              <div tabIndex={0} role="button" className=" bg-violet-400 m-1">
+                <IoIosArrowDown className="text-2xl" />
+              </div>
+              <ul
+                tabIndex={0}
+                className="dropdown-content z-[1] menu p-2 bg-violet-300 rounded-lg w-32"
+              >
+                <li onClick={() => handleFilter("All")}>
+                  <a>All</a>
+                </li>
+                <li onClick={() => handleFilter("Easy")}>
+                  <a>Easy</a>
+                </li>
+                <li onClick={() => handleFilter("Medium")}>
+                  <a>Medium</a>
+                </li>
+                <li onClick={() => handleFilter("Hard")}>
+                  <a>Hard</a>
+                </li>
+              </ul>
             </div>
-            {/* Filter */}
-            <div className="flex justify-center">
-                <div className="flex justify-center w-6/12 divide-x rounded bg-violet-400 text-gray-800 divide-gray-700">
-                    <button type="button" className="px-4 lg:px-8 lg:py-3 font-semibold text-lg flex items-center gap-3"><PiUserList className="text-2xl hidden" /> Filtered Assignments</button>
-                    <button type="button" title="Toggle dropdown" className="p-3">
-                        <div className="dropdown">
-                            <div tabIndex={0} role="button" className=" bg-violet-400 m-1"><IoIosArrowDown className="text-2xl" /></div>
-                            <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 bg-violet-300 rounded-lg w-32">
-                                <li onClick={() => handleFilter("All")}><a>All</a></li>
-                                <li onClick={() => handleFilter("Easy")}><a>Easy</a></li>
-                                <li onClick={() => handleFilter("Medium")}><a>Medium</a></li>
-                                <li onClick={() => handleFilter("Hard")}><a>Hard</a></li>
-                            </ul>
-                        </div>
-                    </button>
-                </div>
-            </div>
-
-            <div className="grid grid-cols-1 items-center md:grid-cols-2 lg:grid-cols-3 gap-7 mt-10 mb-12">
-                {
-                    displayAssign.map(assignment =>
-                        <div key={assignment._id} className="flex h-[590px] flex-col  rounded-2xl shadow-2xl border-2 ">
-                            <img src={assignment.thumbnailImageURL} alt="" className="object-cover object-center w-full rounded-t-md h-80" />
-                            <div className="flex flex-col justify-between p-6 space-y-8">
-                                <div className="space-y-2">
-                                    <h2 className="text-3xl font-semibold tracking-wide text-purple-900 font-serif">{assignment.title.slice(0,20)}</h2>
-                                </div>
-                                <div className='space-y-1'>
-                                    <div className='flex items-center justify-between'>
-                                        <div className="flex items-center"><RiPriceTagLine className='mr-2'></RiPriceTagLine> <span className='font-bold mr-2'>Marks:</span> <span className='font-extrabold'>{assignment.marks}</span></div>
-                                        <div>
-                                            <Link to={`/update/${assignment._id}`}> <button className="p-2 bg-gray-200 rounded-xl hover:bg-gray-300" title="Update Assignment"><GrEdit className="text-lg" /></button></Link>
-                                        </div>
-                                    </div>
-
-                                    <div className="flex items-center justify-between">
-                                        <div className='flex items-center'><FaRegStar className='mr-2'></FaRegStar><span className='font-bold mr-2'>DiffiltyLevel:</span> <span className='font-extrabold'>{assignment.difficultyLevel}</span></div>
-                                        <div>
-                                            <button onClick={() => handleDelete(assignment._id, assignment.userEmail)} className="p-2 bg-gray-200 rounded-xl hover:bg-gray-300" title="Delete Assignment"><RiDeleteBin6Line className="text-lg" /></button>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="">
-                                    <Link to={`/viewAssignment/${assignment._id}`}> <button type="button" className="flex btn  items-center justify-center w-full p-3 font-extrabold tracking-wide rounded-md "><span className='text-blue-400'><TbListDetails className='text-xl' /></span> View Assignment</button></Link>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                }
-            </div>
+          </button>
         </div>
-    );
+      </div>
+
+      <div className="grid grid-cols-1 items-center md:grid-cols-2 lg:grid-cols-3 gap-7 mt-10 mb-12">
+        {displayAssign.map((assignment) => (
+          <div
+            key={assignment._id}
+            className="flex h-[590px] flex-col  rounded-2xl shadow-2xl border-2 "
+          >
+            <img
+              src={assignment.thumbnailImageURL}
+              alt=""
+              className="object-cover object-center w-full rounded-t-md h-80"
+            />
+            <div className="flex flex-col justify-between p-6 space-y-8">
+              <div className="space-y-2">
+                <h2 className="text-3xl font-semibold tracking-wide text-purple-900 font-serif">
+                  {assignment.title.slice(0, 20)}
+                </h2>
+              </div>
+              <div className="space-y-1">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center">
+                    <RiPriceTagLine className="mr-2"></RiPriceTagLine>{" "}
+                    <span className="font-bold mr-2">Marks:</span>{" "}
+                    <span className="font-extrabold">{assignment.marks}</span>
+                  </div>
+                  <div>
+                    <Link to={`/update/${assignment._id}`}>
+                      {" "}
+                      <button
+                        className="p-2 bg-gray-200 rounded-xl hover:bg-gray-300"
+                        title="Update Assignment"
+                      >
+                        <GrEdit className="text-lg" />
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center">
+                    <FaRegStar className="mr-2"></FaRegStar>
+                    <span className="font-bold mr-2">DiffiltyLevel:</span>{" "}
+                    <span className="font-extrabold">
+                      {assignment.difficultyLevel}
+                    </span>
+                  </div>
+                  <div>
+                    <button
+                      onClick={() =>
+                        handleDelete(assignment._id, assignment.userEmail)
+                      }
+                      className="p-2 bg-gray-200 rounded-xl hover:bg-gray-300"
+                      title="Delete Assignment"
+                    >
+                      <RiDeleteBin6Line className="text-lg" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="">
+                <Link to={`/viewAssignment/${assignment._id}`}>
+                  {" "}
+                  <button
+                    type="button"
+                    className="flex btn  items-center justify-center w-full p-3 font-extrabold tracking-wide rounded-md "
+                  >
+                    <span className="text-blue-400">
+                      <TbListDetails className="text-xl" />
+                    </span>{" "}
+                    View Assignment
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Assignment;
--- src/Pages/CreateAssignment.jsx
+++ src/Pages/CreateAssignment.jsx
@@ -4,95 +4,159 @@ import "react-datepicker/dist/react-datepicker.css";
 import Swal from "sweetalert2";
 import { AuthContext } from "../Components/AuthProvider";
 
-
 const CreateAssignment = () => {
-    const {user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
-    const [startDate, setStartDate] = useState(new Date());
-    const handleCreate = e =>{
-        e.preventDefault();
-        const form = e.target;
-        const title = form.title.value;
-        const thumbnailImageURL = form.imageUrl.value;
-        const description = form.description.value;
-        const difficultyLevel = form.difficultyLevel.value;
-        const marks = form.mark.value;
-        const date = form.date.value;
-        const userEmail = user.email
-        const assignment = {title, thumbnailImageURL, description, difficultyLevel, marks, date, userEmail};
-        console.log(assignment);
+  const [startDate, setStartDate] = useState(new Date());
+  const handleCreate = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const title = form.title.value;
+    const thumbnailImageURL = form.imageUrl.value;
+    const description = form.description.value;
+    const difficultyLevel = form.difficultyLevel.value;
+    const marks = form.mark.value;
+    const date = form.date.value;
+    const userEmail = user.email;
+    const assignment = {
+      title,
+      thumbnailImageURL,
+      description,
+      difficultyLevel,
+      marks,
+      date,
+      userEmail,
+    };
+    console.log(assignment);
 
-        //send assignment to server
-        fetch(`${import.meta.env.VITE_API_URL}/assignments`, {
-           method: 'POST',
-           headers: {
-            'content-type' : 'application/json'
-           },
-           body: JSON.stringify(assignment) 
-        })
-        .then(res => res.json())
-        .then (data => {
-            console.log(data)
-            if (data.insertedId) {
-                Swal.fire({
-                    title: 'Success!',
-                    text: 'Assignment Created Successfully!',
-                    icon: 'success',
-                    confirmButtonText: 'Done'
-                })
-                form.reset()
-            }
-        })
-    }
-    return (
-        <div className="p-4 lg:mx-24">
-            <section className="lg:p-24 bg-gray-300 text-gray-50 mb-8">
-                <form onSubmit={handleCreate} noValidate="" action="" className="container flex flex-col mx-auto space-y-12">
-                    <fieldset className="grid grid-cols-4 gap-6 p-6 lg:p-16 rounded-md shadow-sm bg-gray-600">
-                        <div className="space-y-2 col-span-full lg:col-span-1">
-                            <p className="font-medium text-3xl ">Create <br /> an Assignment</p>
-                            <p className="text-xs">Credibly create dynamic assignment after long-term high-impact infrastructures. Synergistically provide.</p>
-                        </div>
-                        <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
-                            <div className="col-span-full sm:col-span-3">
-                                <label htmlFor="firstname" className="text-sm">Title</label>
-                                <input id="firstname" name="title" type="text" placeholder="Give a title" className="w-full input rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-violet-400 border-gray-700" />
-                            </div>
-                            <div className="col-span-full sm:col-span-3">
-                                <label htmlFor="lastname" className="text-sm">Thumbnail Image URL</label>
-                                <input id="lastname" name="imageUrl" type="text" placeholder="Image URL" className="w-full input rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-violet-400 border-gray-700" />
-                            </div>
-                            <div className="col-span-full sm:col-span-3">
-                                <label htmlFor="email" className="text-sm">Description</label>
-                                <input id="email" name="description" type="text" placeholder="Description" className="w-full textarea rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-violet-400 border-gray-700" />
-                            </div>
-                            <div className="col-span-full sm:col-span-3">
-                                <label htmlFor="city" className="text-sm">Difficulty Level</label>
-                                <select name="difficultyLevel" className="w-full rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-violet-400 border-gray-700 input">
-                                    <option value="Easy">Easy</option>
-                                    <option value="Medium">Medium</option>
-                                    <option value="Hard">Hard</option>
-                                </select>
-                                
-                            </div>
-                            <div className="col-span-full sm:col-span-3">
-                                <label htmlFor="state" className="text-sm">Marks</label>
-                                <input id="state" type="text" name="mark" placeholder="Marks" className="w-full rounded-md focus:ring focus:ring-opacity-75 input text-gray-900 focus:ring-violet-400 border-gray-700" />
-                            </div>
-                            <div className="col-span-full sm:col-span-3 flex flex-col mt-1">
-                                <label htmlFor="zip" className="text-sm">Date</label>
-                                <ReactDatePicker className="w-full rounded-md focus:ring focus:ring-opacity-75 input text-gray-900 focus:ring-violet-400 border-gray-700" name="date" selected={startDate} onChange={(date) => setStartDate(date)} />  
-                            </div>
-                            <div className="col-span-full">
-                                <button type="submit" className="btn w-full mt-10 bg-slate-300 font-extrabold">Create</button> 
-                            </div>
-                        </div>
-                    </fieldset>
-                  
-                </form>
-            </section>
-        </div>
-    );
+    //send assignment to server
+    fetch(`${import.meta.env.VITE_API_URL}/assignments`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(assignment),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Success!",
+            text: "Assignment Created Successfully!",
+            icon: "success",
+            confirmButtonText: "Done",
+          });
+          form.reset();
+        }
+      });
+  };
+  return (
+    <div className="p-4 lg:mx-24">
+      <section className="lg:p-24 bg-gray-300 text-gray-50 mb-8">
+        <form
+          onSubmit={handleCreate}
+          noValidate=""
+          action=""
+          className="container flex flex-col mx-auto space-y-12"
+        >
+          <fieldset className="grid grid-cols-4 gap-6 p-6 lg:p-16 rounded-md shadow-sm bg-gray-600">
+            <div className="space-y-2 col-span-full lg:col-span-1">
+              <p className="font-medium text-3xl ">
+                Create <br /> an Assignment
+              </p>
+              <p className="text-xs">
+                Credibly create dynamic assignment after long-term high-impact
+                infrastructures. Synergistically provide.
+              </p>
+            </div>
+            <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="firstname" className="text-sm">
+                  Title
+                </label>
+                <input
+                  id="firstname"
+                  name="title"
+                  type="text"
+                  placeholder="Give a title"
+                  className="w-full input rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-violet-400 border-gray-700"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="lastname" className="text-sm">
+                  Thumbnail Image URL
+                </label>
+                <input
+                  id="lastname"
+                  name="imageUrl"
+                  type="text"
+                  placeholder="Image URL"
+                  className="w-full input rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-violet-400 border-gray-700"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="email" className="text-sm">
+                  Description
+                </label>
+                <input
+                  id="email"
+                  name="description"
+                  type="text"
+                  placeholder="Description"
+                  className="w-full textarea rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-violet-400 border-gray-700"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="city" className="text-sm">
+                  Difficulty Level
+                </label>
+                <select
+                  name="difficultyLevel"
+                  className="w-full rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-violet-400 border-gray-700 input"
+                >
+                  <option value="Easy">Easy</option>
+                  <option value="Medium">Medium</option>
+                  <option value="Hard">Hard</option>
+                </select>
+              </div>
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="state" className="text-sm">
+                  Marks
+                </label>
+                <input
+                  id="state"
+                  type="text"
+                  name="mark"
+                  placeholder="Marks"
+                  className="w-full rounded-md focus:ring focus:ring-opacity-75 input text-gray-900 focus:ring-violet-400 border-gray-700"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-3 flex flex-col mt-1">
+                <label htmlFor="zip" className="text-sm">
+                  Date
+                </label>
+                <ReactDatePicker
+                  className="w-full rounded-md focus:ring focus:ring-opacity-75 input text-gray-900 focus:ring-violet-400 border-gray-700"
+                  name="date"
+                  selected={startDate}
+                  onChange={(date) => setStartDate(date)}
+                />
+              </div>
+              <div className="col-span-full">
+                <button
+                  type="submit"
+                  className="btn w-full mt-10 bg-slate-300 font-extrabold"
+                >
+                  Create
+                </button>
+              </div>
+            </div>
+          </fieldset>
+        </form>
+      </section>
+    </div>
+  );
 };
 
 export default CreateAssignment;
