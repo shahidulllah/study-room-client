4dc017a916a40008e0ca7f205f107ca6233c8458
shahidulllah <mdshahidsumon177@gmail.com>
2025-11-01 20:01:46 +0600

    working

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,21 +4,20 @@ import Feature from "./Feature";
 import FeatureCard from "./FeatureCard";
 import Team from "./Team";
 
-
 const Home = () => {
-    return (
-        <div>
-            <div>
-            <Banner></Banner>
-            </div>
-            <div className="p-4 lg:mx-24">
-                <Feature></Feature>
-                <FeatureCard></FeatureCard>
-                <Faq></Faq>
-                <Team></Team>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <Banner></Banner>
+      </div>
+      <div className="p-4 lg:mx-24">
+        <Feature></Feature>
+        <FeatureCard></FeatureCard>
+        <Faq></Faq>
+        <Team></Team>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
